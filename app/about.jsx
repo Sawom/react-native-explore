e2984a3b6c4cdf0900740ff92ff92384c3fb457a
Sawom <asawom250@gmail.com>
2026-08-23 14:04:18 +0600

    theme dark light

--- app/about.jsx
+++ app/about.jsx
@@ -1,33 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
 import { Link } from 'expo-router'
+import { View, Text, StyleSheet } from 'react-native'
+import { Colors } from "../constant/Colors.js"
+import { useColorScheme } from "react-native"
 
 const About = () => {
+  const colorScheme = useColorScheme()
+  const theme = Colors[colorScheme] ?? Colors.light
+
   return (
-    <View style={styles.container} >
-        <Text style={styles.title} href="/contact" >Contact Page</Text>
-        <Link href="/about" style={styles.link} > About page </Link>
-        <Link href="/" style={styles.link}>Home Page</Link>
+    <View style={[styles.container, {backgroundColor: theme.background}]}>
+      <Text style={[styles.title, {color: theme.title}]}>About Page</Text>
+
+      <Link href="/" style={[styles.link, {color: theme.text}]}>Home Page</Link>
     </View>
   )
 }
+
 export default About
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#e0dfe8',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-
-    link: {
-        marginVertical: 10,
-        borderBottomWidth: 1
-    }
+  container: {
+    backgroundColor: '#e0dfe8',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
+  }
 })
-
