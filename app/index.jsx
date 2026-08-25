d4bd63434c0f5048690cddced0b759c36b1e7a63
Sawom <asawom250@gmail.com>
2026-08-25 19:56:07 +0600

    acrollable content added

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 import { Link } from "expo-router";
 
 import ThemedView from "../components/ThemedView.jsx";
@@ -9,36 +9,41 @@ import Spacer from "../components/Spacer.jsx";
 const Home = () => {
   return (
     <ThemedView style={styles.container}>
-      <ThemedLogo />
-      <Spacer />
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false} // ডানপাশের স্ক্রোল বার হাইড করতে চাইলে (অপশনাল)
+      >
+        <ThemedLogo />
+        <Spacer />
 
-      <ThemedText style={styles.title} title={true}>
-        The Number 1
-      </ThemedText>
+        <ThemedText style={styles.title} title={true}>
+          The Number 1
+        </ThemedText>
 
-      <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
-        Reading List App
-      </ThemedText>
+        <ThemedText style={{ marginTop: 10, marginBottom: 30 }}>
+          Reading List App
+        </ThemedText>
 
-      <Link href="/about" style={styles.link}>
-        <ThemedText>About Page</ThemedText>
-      </Link>
+        <Link href="/about" style={styles.link}>
+          <ThemedText>About Page</ThemedText>
+        </Link>
 
-      <Link href="/contact" style={styles.link}>
-        <ThemedText>Contact Page</ThemedText>
-      </Link>
+        <Link href="/contact" style={styles.link}>
+          <ThemedText>Contact Page</ThemedText>
+        </Link>
 
-      <Link href="/login" style={styles.link}>
-        <ThemedText>Login</ThemedText>
-      </Link>
+        <Link href="/login" style={styles.link}>
+          <ThemedText>Login</ThemedText>
+        </Link>
 
-      <Link href="/register" style={styles.link}>
-        <ThemedText>Register</ThemedText>
-      </Link>
+        <Link href="/register" style={styles.link}>
+          <ThemedText>Register</ThemedText>
+        </Link>
 
-      <Link href="/profile" style={styles.link}>
-        <ThemedText> Profile </ThemedText>
-      </Link>
+        <Link href="/profile" style={styles.link}>
+          <ThemedText> Profile </ThemedText>
+        </Link>
+      </ScrollView>
     </ThemedView>
   );
 };
@@ -54,12 +59,16 @@ const styles = StyleSheet.create({
   img: {
     marginVertical: 20,
   },
+  scrollContent: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
   title: {
     fontWeight: "bold",
     fontSize: 18,
   },
   link: {
     marginVertical: 10,
-    borderBottomWidth: 1,
+    textDecorationLine: "none",
   },
 });
