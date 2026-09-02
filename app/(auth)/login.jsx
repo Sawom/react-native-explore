b1bfb5ed6adc08ae193ff4a616d874aae3d767ad
Sawom <asawom250@gmail.com>
2026-09-02 22:17:50 +0600

    login updated

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -5,20 +5,29 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import { Link } from "expo-router";
-
+import { useState } from "react";
 import ThemedView from "../../components/ThemedView.jsx";
 import ThemedText from "../../components/ThemedText.jsx";
 import Spacer from "../../components/Spacer.jsx";
 import ThemedButton from "../../components/ThemedButton.jsx";
 import ThemedTextInput from "../../components/ThemedTextInput.jsx";
-import { useState } from "react";
+
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+
+  const { user, login } = useUser();
 
   const handleSubmit = async () => {
-    console.log("login form submitted: ", email, password);
+    setError(null);
+
+    try {
+      await login(email, password);
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
@@ -33,7 +42,7 @@ const Login = () => {
 
         <Spacer />
         <ThemedTextInput
-          style={{ marginBottom: 20, width: "80%", alignSelf: "center" }}
+          style={{ marginBottom: 20, width: "80%" }}
           placeholder="Email"
           value={email}
           onChangeText={setEmail}
@@ -41,19 +50,19 @@ const Login = () => {
         />
 
         <ThemedTextInput
-          style={{ marginBottom: 20, width: "80%", alignSelf: "center" }}
+          style={{ marginBottom: 20, width: "80%" }}
           placeholder="Password"
           value={password}
           onChangeText={setPassword}
           secureTextEntry
         />
 
-        <ThemedButton
-          onPress={handleSubmit}
-          style={{ marginBottom: 20, width: "50%", alignSelf: "center" }}
-        >
-          <Text style={{ color: "#f2f2f2", alignSelf: "center" }}>Login</Text>
+        <ThemedButton onPress={handleSubmit}>
+          <Text style={{ color: "#f2f2f2" }}>Login</Text>
         </ThemedButton>
+
+        <Spacer />
+        {error && <Text style={styles.error}>{error}</Text>}
 
         <Spacer height={100} />
         <Link href="/register" replace>
@@ -72,10 +81,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  error: {
+    color: Colors.warning,
+    padding: 10,
+    backgroundColor: "#f5c1c8",
+    borderColor: Colors.warning,
+    borderWidth: 1,
+    borderRadius: 6,
+    marginHorizontal: 10,
   },
 });
--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,13 +2,14 @@ import { Stack } from "expo-router";
 import { Colors } from "../constant/Colors.js";
 import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { UserProvider } from "../contexts/UserContext.jsx";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
-    <>
+    <UserProvider>
       <StatusBar value="auto" />
       <Stack
         screenOptions={{
@@ -16,14 +17,13 @@ export default function RootLayout() {
           headerTintColor: theme.title,
         }}
       >
+        {/* Groups */}
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+
         {/* Individual Screens */}
         <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen
-          name="contact"
-          options={{ title: "Contact", headerShown: false }}
-        />
       </Stack>
-    </>
+    </UserProvider>
   );
 }
