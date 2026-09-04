cc2cb1297f3b2ee713543f1241e5ba29e4dd848d
Sawom <asawom250@gmail.com>
2026-09-04 19:09:10 +0600

    root layout updated

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,7 @@ import { Colors } from "../constant/Colors.js";
 import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { UserProvider } from "../contexts/UserContext.jsx";
+import { BooksProvider } from "../contexts/BooksContext.jsx";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -10,20 +11,25 @@ export default function RootLayout() {
 
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-        }}
-      >
-        {/* Groups */}
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          {/* Groups */}
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
 
-        {/* Individual Screens */}
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-      </Stack>
+          {/* Individual Screens */}
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+
+          {/* Book details screen */}
+          {/* <Stack.Screen name="books/[id]" options={} */}
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 }
